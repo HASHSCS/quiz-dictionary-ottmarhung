22567fc807719618fb07669bca73241877385a8c
Ottmar <ottmarhung@gmail.com>
2023-10-16 11:16:56 +0800

    asasasadsfge

--- solutions/dicts.py
+++ solutions/dicts.py
@@ -1,12 +1,17 @@
 def get_value(data_dict, key):
-    """
-    Returns the value for the given key in the dictionary.
-    If the key is not in the dictionary, return None.
-    """
-    pass #Replace with your code
+    if key in data_dict:
+        return data_dict[key]
+    else:
+        return None
+  #Replace with your code
 
 
 def remove_key(data_dict, key):
+    if key in data_dict:
+        del data_dict[key] 
+        return data_dict
+    else:
+        None
     """
     Removes the entry with the specified key from the dictionary.
     If the key is not found, the dictionary should remain unchanged.
